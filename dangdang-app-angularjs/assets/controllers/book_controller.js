3f434ed06f0f1f6a0ae3e7552fcf1700cb5c3788
Shmily502461009 <Mac@yingzi.local>
2016-10-19 17:11:01 +0800

    添加图片

--- dangdang-app-angularjs/assets/controllers/book_controller.js
+++ dangdang-app-angularjs/assets/controllers/book_controller.js
@@ -1,8 +1,8 @@
 app.controller('bookController',['$scope',function($scope){
     $scope.bookTypes = [
-        {id:'ertong',name:'儿童',img:'',description:''},
-        {id:'lishi',name:'历史',img:'',description:''},
-        {id:'dongman',name:'动漫',img:'',description:''},
-        {id:'qingchunwenxue',name:'青春文学',img:'',description:''}
+        {id:'ertong',name:'儿童',img:'http://e.hiphotos.baidu.com/zhidao/wh%3D450%2C600/sign=fe006f4be950352ab1342d0c6673d7c4/267f9e2f07082838dbc3bce8b899a9014d08f104.jpg',description:'当当网热销图书'},
+        {id:'lishi',name:'历史',img:'http://e.hiphotos.baidu.com/zhidao/wh%3D450%2C600/sign=fe006f4be950352ab1342d0c6673d7c4/267f9e2f07082838dbc3bce8b899a9014d08f104.jpg',description:'当当网热销图书'},
+        {id:'dongman',name:'动漫',img:'http://e.hiphotos.baidu.com/zhidao/wh%3D450%2C600/sign=fe006f4be950352ab1342d0c6673d7c4/267f9e2f07082838dbc3bce8b899a9014d08f104.jpg',description:'当当网热销图书'},
+        {id:'qingchunwenxue',name:'青春文学',img:'http://e.hiphotos.baidu.com/zhidao/wh%3D450%2C600/sign=fe006f4be950352ab1342d0c6673d7c4/267f9e2f07082838dbc3bce8b899a9014d08f104.jpg',description:'当当网热销图书'}
     ]
 }])
