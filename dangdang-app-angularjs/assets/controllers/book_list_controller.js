f9321652832c6fe95b9bba1b46abf5f092b29b1e
Shmily502461009 <Mac@yingzi.local>
2016-10-19 18:01:00 +0800

    修改获取数据方式

--- dangdang-app-angularjs/assets/controllers/book_list_controller.js
+++ dangdang-app-angularjs/assets/controllers/book_list_controller.js
@@ -1,14 +1,15 @@
 app.controller('bookListController',['$scope','commonService','$routeParams',function($scope,c_service,$routeParams){
+    $scope.data = [];
     c_service.getData('ertong',function(res){
-        $scope.data = res.data.data;
+        $scope.data = res.data;
     });
     c_service.getData('lishi',function(res){
-        $scope.data = res.data.data;
+        $scope.data = res.data;
     });
     c_service.getData('dongman',function(res){
-        $scope.data = res.data.data;
+        $scope.data = res.data;
     });
     c_service.getData('qingchunwenxue',function(res){
-        $scope.data = res.data.data;
+        $scope.data = res.data;
     });
 }])
